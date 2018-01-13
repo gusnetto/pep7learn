284abfeb9e6e92c4e891559835b0b88d07a38a29
Gustavo Rocha <gustavo.rochanetto@gmail.com>
2018-01-13 01:50:37 -0200

    switch case

--- estudophp/estruturacontrole/if.php
+++ estudophp/estruturacontrole/if.php
@@ -1,13 +1,31 @@
 <?php 
 $qualSuaIdade = 30;
 $idadeCrianca = 12;
-$idadeAdulto = 18;
+$idadeMaior = 18;
 $idadeMelhor = 55;
 
-if ($qualSuaIdade < $idadeCrianca) {
+if ($qualSuaIdade < $idadeCrianca){
+
+	echo("Criança");
+
+} else if ($qualSuaIdade < $idadeMaior ) {
+
+	echo("Adolescente");
+		
+
+}else if ($qualSuaIdade < $idadeMelhor) {
+
+	echo(" Adulto");
+
+}else{
 	
-	echo("Criança")
+	echo("Idoso");
 }
+
+echo("<br>");
+
+//operador ternário ? então  : senão  instrucão simples 
+echo($qualSuaIdade < $idadeMaior) ? "Mmenor de idade ": "Maior de idade";
 
 
  ?>
